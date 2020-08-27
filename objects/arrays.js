3962603a4f96155144924a478e330a37521df5df
Garbiñe <gboned@cifpfbmoll.eu>
2020-08-27 21:25:32 +0200

    Añadir ejercicios de cadenas y funciones dentro de objetos

---
+++ objects/arrays.js
@@ -0,0 +1,12 @@
+var cool_array = [
+    "hello world",
+    42,
+    true
+];
+
+console.log(cool_array[0]);
+console.log(cool_array[1]);
+console.log(cool_array[2]);
+
+console.log(cool_array.length);
+console.log(typeof cool_array);
