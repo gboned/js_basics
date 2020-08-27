ad905b04e18fb902de4d989fae737258839bf287
gboned <gboned@cifpfbmoll.eu>
2020-08-27 22:46:57 +0200

    Añadir comentarios

--- variables/let_keyword.js
+++ variables/let_keyword.js
@@ -1,15 +1,16 @@
 function foo() {
 
-    var a = 1;
+    var a = 1; // Valor inicial de a
 
     if (a >= 1) {
-        let b = 2;
+        let b = 2; // a es igual a 1, así
+                   // que b es igual a 2
 
         while (b < 5) {
-            let c = b * 2;
-            b++;
+            let c = b * 2; // c = 4 // 6 // 8
+            b++; // 2 + 1 // 3 + 1 // 4 + 1
 
-            console.log( a + c );
+            console.log( a + c ); // 5, 7, 9
         }
     }
 }
