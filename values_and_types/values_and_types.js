fcb7c64c08bcec5e8e2adc7b1f0f44b2cf6eb606
Garbiñe <gboned@cifpfbmoll.eu>
2020-08-27 21:23:29 +0200

    Añadir ejercicios de valores y tipos

---
+++ values_and_types/values_and_types.js
@@ -0,0 +1,41 @@
+var a;
+console.log(typeof a);
+
+var b = typeof a;
+console.log(typeof b);
+
+a = "hello world";
+console.log(typeof a);
+
+b = typeof a;
+console.log(typeof b);
+
+a = 42;
+console.log(typeof a);
+
+b = typeof a;
+console.log(typeof b);
+
+a = true;
+console.log(typeof a);
+
+b = typeof a;
+console.log(typeof b);
+
+a = null;
+console.log(typeof a);
+
+b = typeof a;
+console.log(typeof b);
+
+a = undefined;
+console.log(typeof a);
+
+b = typeof a;
+console.log(typeof b);
+
+a = {b: "c"};
+console.log(typeof a);
+
+b = typeof a;
+console.log(typeof b);
